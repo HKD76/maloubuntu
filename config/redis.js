133db1291eeb11a54a68347977b79c6661ffe1ab
HKD76 <hugocadet76750@gmail.com>
2025-01-22 15:42:18 +0100

    yes

--- config/redis.js
+++ config/redis.js
@@ -4,7 +4,7 @@ import logger from "./logger.js";
 const redis = new Redis({
   host: process.env.REDIS_HOST || "localhost",
   port: process.env.REDIS_PORT || 6379,
-  password: process.env.REDIS_PASSWORD,
+  password: "Toto4242@#",
   retryStrategy: (times) => {
     const delay = Math.min(times * 50, 2000);
     return delay;

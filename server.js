b1de6f6a7861213e5ca5901963f2a4107e665851
HKD76 <hugocadet76750@gmail.com>
2025-01-22 15:32:45 +0100

    test

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ import Redis from "ioredis";
 const redis = new Redis({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD,
+  password: "Toto4242@#",
 });
 
 redis.on("connect", () => {
